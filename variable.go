b3336ae44b2af178e282205e360c89d96f37c486
Alfredo España <alfespa17@gmail.com>
2026-07-28 18:01:07 -0600

    feat: update jsonapi struct tags for Workspace and Variable models and add omission test coverage

--- variable.go
+++ variable.go
@@ -7,16 +7,16 @@ type Variable struct {
 	ID          string     `jsonapi:"primary,variable"`
 	Key         string     `jsonapi:"attr,key"`
 	Value       string     `jsonapi:"attr,value"`
-	Description string     `jsonapi:"attr,description"`
+	Description string     `jsonapi:"attr,description,omitempty"`
 	Category    string     `jsonapi:"attr,category"`
 	Sensitive   bool       `jsonapi:"attr,sensitive"`
 	Hcl         bool       `jsonapi:"attr,hcl"`
-	Incomplete  bool       `jsonapi:"attr,incomplete"`
-	CreatedBy   *string    `jsonapi:"attr,createdBy"`
-	CreatedDate *string    `jsonapi:"attr,createdDate"`
-	UpdatedBy   *string    `jsonapi:"attr,updatedBy"`
-	UpdatedDate *string    `jsonapi:"attr,updatedDate"`
-	Workspace   *Workspace `jsonapi:"relation,workspace,omitempty"`
+	Incomplete  bool       `jsonapi:"attr,incomplete,omitempty"`
+	CreatedBy   *string    `jsonapi:"attr,createdBy,omitempty"`
+	CreatedDate *string    `jsonapi:"attr,createdDate,omitempty"`
+	UpdatedBy   *string    `jsonapi:"attr,updatedBy,omitempty"`
+	UpdatedDate *string    `jsonapi:"attr,updatedDate,omitempty"`
+	Workspace   *Workspace `jsonapi:"relation,workspace"`
 }
 
 // VariableService handles communication with the workspace variable endpoints.
